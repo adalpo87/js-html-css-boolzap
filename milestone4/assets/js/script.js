87ad679c75626adae216472f20caf634efdd0d01
adalpo87 <adalpo87@gmail.com>
2021-05-11 13:22:49 +0200

    completato milestone 4

---
+++ milestone4/assets/js/script.js
@@ -0,0 +1,150 @@
+
+ const app = new Vue({
+
+    el: '#app',
+
+    data:{
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        text: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+                visible: true,
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+                visible: true,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+        ],
+        
+        activeIndex: 0,
+        myText: '',
+        nameText: '',
+        timeNow: dayjs().format('DD/MM/YYYY H-mm-ss'),
+        timeNow2: dayjs().format('H-mm')
+
+    },
+    
+    methods:{
+
+        avatarImg(avatar){
+            return `assets/img/avatar${avatar}.jpg`
+        },
+
+        newMessage(){
+            this.contacts[this.activeIndex].messages.push(
+                {
+                    date: this.timeNow,
+                    text: this.myText,
+                    status: 'sent'
+                });
+            this.myText = '';
+            setTimeout(()=>{
+                this.contacts[this.activeIndex].messages.push(
+                    {
+                        date: this.timeNow,
+                        text: 'ok!',
+                        status: 'receveid'
+                    });
+            }, 1000);
+            
+        },
+        /* mette in paragone i due array 
+        si crea un ciclo for dove il limite è dato dal mio input*/
+
+    }
+
+    
+
+ })
+
+/* che apparirà dopo 1 secondo.
+Milestone 4
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, 
+vengono visualizzati solo i contatti il cui nome contiene le
+lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” 
+rimangono solo Marco e Martina)
+
+
+
+soluzione
+
+tramite v-model catturo l inserimento lettere
+creo una variabile messaggio vuota '' dove verrà salvato l input.
+
+devo ciclare i nomi
+
+ */
